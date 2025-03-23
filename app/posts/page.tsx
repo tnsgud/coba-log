acf7f47b771c456a24b7d3a5482e55927f47a256
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-23 07:27:38 +0000

    💄 Update posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,29 @@
+import { Badge } from "@/components/ui/badge";
+
+const posts = [
+  {
+    id: 1,
+    title: "This is test title",
+    description: "this is test description",
+    tags: [
+      { id: 1, name: "programming" },
+      { id: 2, name: "math" },
+    ],
+  },
+];
+
 export default function PostsPage() {
-  return <div>this is posts page</div>;
+  return (
+    <main className="p-5">
+      {posts.map((post) => (
+        <p key={`post-${post.id}`} className="flex flex-col border-2 px-2">
+          <span className="text-4xl font-bold">{post.title}</span>
+          <span>{post.description}</span>{" "}
+          {post.tags.map((tag) => (
+            <Badge key={`post-${post.id}-tag-${tag.id}`}>{tag.name}</Badge>
+          ))}
+        </p>
+      ))}
+    </main>
+  );
 }
