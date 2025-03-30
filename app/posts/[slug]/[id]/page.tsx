5de42ca09e696d8b8109f929876229def16bfc70
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-30 06:48:35 +0000

    🚩 dynamic route 예시 코드 추가

--- app/posts/[slug]/[id]/page.tsx
+++ app/posts/[slug]/[id]/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <div>this is test</div>;
+interface Props {
+  params: Promise<{ slug: string; id: string }>;
+}
+
+export default async function Page({ params }: Props) {
+  const {slug, id} = await params;
+
+  return <div>this is test post in the {slug} category and the id is {id}</div>;
 }
