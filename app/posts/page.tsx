faf3884cb5370cbb33943e3c5da614ffb3c0861b
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-02 14:14:11 +0000

    💄 Update layout

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,13 +1,13 @@
-import { supabase } from "@/lib/supabase";
+import supabase from "@/lib/supabase";
 import PostCard from "./components/post-card";
 
 export default async function PostsPage() {
   const { data: posts } = await supabase
     .from("post")
-    .select("id, title, description, views, category(id, name, slug)");
+    .select("id, title, description, views, created_at, category(id, name, slug)");
 
   return (
-    <main className="flex flex-col gap-5 p-5">
+    <main className="grid grid-cols-2 gap-5 p-5">
       {posts?.map((post) => <PostCard key={`post-${post.id}`} post={post} />)}
     </main>
   );
