21e798f9b7f0ed38a6dcc29ed01cbff2f2de78c1
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-02 14:14:25 +0000

    🔍 Update SEO

--- app/posts/[slug]/[id]/page.tsx
+++ app/posts/[slug]/[id]/page.tsx
@@ -1,9 +1,39 @@
+import supabase from "@/lib/supabase";
+
 interface Props {
   params: Promise<{ slug: string; id: string }>;
 }
 
-export default async function Page({ params }: Props) {
-  const {slug, id} = await params;
+export const revalidate = 60;
+export const dynamicParams = true;
 
-  return <div>this is test post in the {slug} category and the id is {id}</div>;
+export async function generateStaticParams() {
+  const { data } = await supabase.from("post").select("id, category(slug)");
+
+  return (
+    data?.map((row) => ({ id: `${row.id}`, slug: row.category.slug })) ?? []
+  );
+}
+
+export default async function Page({ params }: Props) {
+  const { slug, id } = await params;
+
+  const { data: post } = await supabase
+    .from("post")
+    .select("*")
+    .eq("id", Number(id))
+    .limit(1)
+    .single();
+
+  if (!post) return <div>this post is null</div>;
+
+  return (
+    <div>
+      this is test post in the {slug} category and the id is {id}
+      <span>{post.title}</span>
+      <span>{post.description}</span>
+      <span>{post.content}</span>
+      <span>{post.views}</span>
+    </div>
+  );
 }
