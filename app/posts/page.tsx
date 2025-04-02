7226a2880eaa9145eae099acf35cec0767e6bebe
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-02 14:46:51 +0000

    💄 Add Floating Button

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,8 @@
 import supabase from "@/lib/supabase";
 import PostCard from "./components/post-card";
 import { Button } from "@/components/ui/button";
-import { Plus } from 'lucide-react';
+import Link from "next/link";
+import { Plus } from "lucide-react";
 
 export default async function PostsPage() {
   const { data: posts } = await supabase
@@ -13,8 +14,14 @@ export default async function PostsPage() {
   return (
     <main className="grid grid-cols-2 gap-5 p-5">
       {posts?.map((post) => <PostCard key={`post-${post.id}`} post={post} />)}
-      <Button className="fixed right-4 bottom-4 z-50 size-15" >
-        <Plus className="stroke-black size-8 fill-black" />
+      <Button
+        className="fixed right-4 bottom-4 z-50 size-15 hover:cursor-pointer"
+        variant="outline"
+        size="icon"
+      >
+        <Link href="/write">
+          <Plus className="size-6" />
+        </Link>
       </Button>
     </main>
   );
