3beecd37a8631314e6cd7d924d90ed4ffb99165e
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-02 14:06:44 +0000

    🍱 Add Icons

--- components/icons/arrow-trending-up.tsx
+++ components/icons/arrow-trending-up.tsx
@@ -1,4 +1,4 @@
-export default function ArrowTrendingUp({ className }: IconDefaultProps) {
+export default function ArrowTrendingUpIcon({ className }: IconDefaultProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
