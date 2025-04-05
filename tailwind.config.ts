61587d129ada3824bcae95b18bbb8518b66ed002
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-05 09:12:48 +0000

    ➕ Add 'react-markdown', 'remark-gfm' and 'tailwindcss/typography' dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwindcss";
+import Typography from '@tailwindcss/typography';
 
 const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  plugins: [Typography]
 };
 
 export default config;
