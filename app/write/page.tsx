0ca5bcd0ceb5bb74a62826f51b78c07f5f1b7efd
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-05 09:13:38 +0000

    💄 Add Inputs

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,3 +1,12 @@
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function WritePage() {
-  return <div>this is Write page</div>;
+  return (
+    <div>
+      <Input type="text" placeholder="Title" required />
+      <Input type="text" placeholder="Subtitle" required />
+      <Textarea />
+    </div>
+  );
 }
