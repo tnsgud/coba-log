851c9ec4e17164be920d0351cf214c6ce00de7df
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-05 11:20:13 +0000

    🚩 Add post insert

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,12 +1,98 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import CategorySelector from "@/components/category-selector";
+import supabase from "@/lib/supabase";
+
+const formSchema = z.object({
+  title: z.string().min(1, "제목은 필수 요소 입니다."),
+  description: z.string().min(1, "설명은 필수 요소 입니다."),
+  content: z.string().min(1, "내용은 필수 요소 입니다."),
+  category: z.coerce.number().min(1, "카테고리는 필수 요소 입니다."),
+});
 
 export default function WritePage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { title: "", description: "", content: "" },
+  });
+
+  async function onSubmit(value: z.infer<typeof formSchema>) {
+    const { title, description, content, category: category_id } = value;
+
+    const result = await supabase
+      .from("post")
+      .insert({ title, description, content, category_id });
+
+    console.log(result);
+  }
+
   return (
-    <div>
-      <Input type="text" placeholder="Title" required />
-      <Input type="text" placeholder="Subtitle" required />
-      <Textarea />
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-5">
+        {/* TODO:Refactoring 필요 */}
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="description" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <CategorySelector {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea placeholder="content" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
