8f6fac471812076dbc42b572c041d8810a239b22
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-12 14:17:25 +0000

    🔐 Update env name

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,8 @@ import { Database } from "@/database.types";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
