ea01b914e13f5ad1a5066dd78abcdcfdfe8af025
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-21 07:10:57 +0000

    💄 정렬추가 및 렌더링 방식 변경

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,15 +2,18 @@ import supabase from '@/lib/supabase'
 import PostCard from './components/post-card'
 import FloatingButton from './components/floating-button'
 
+export const dynamic = 'force-dynamic'
+
 export default async function PostsPage() {
 	const { data: posts } = await supabase
 		.from('post')
 		.select(
 			'id, title, description, views, created_at, category(id, name, slug)',
 		)
+		.order('created_at', { ascending: false })
 
 	return (
-		<main className="grid grid-cols-2 gap-5 p-5">
+		<main className="grid grid-cols-1 gap-5 p-5">
 			{posts?.map((post) => (
 				<PostCard key={`post-${post.id}`} post={post} />
 			))}
